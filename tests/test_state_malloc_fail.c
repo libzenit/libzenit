e13ec18462fd0f3691a4218d65aa2c260e404824
Ian Torres <iantorres@outlook.com>
2026-07-16 13:24:58 -0400

    WIP

--- tests/test_state_malloc_fail.c
+++ tests/test_state_malloc_fail.c
@@ -27,7 +27,7 @@ void *__wrap_malloc(size_t size) {
 
 int main(void) {
     zenit_state_transition_t t = {0, 0, 0, NULL};
-    zenit_state_t *state = zenit_state_allocate(&t, 1, 0);
+    const zenit_state_t *state = zenit_state_allocate(&t, 1, 0);
     if (state == NULL) {
         printf("PASS: malloc failure returns NULL\n");
         return 0;
