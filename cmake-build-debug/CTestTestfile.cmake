431d84a8493254c87b327f528cb45bf2d0c85002
Ian Torres <iantorres@outlook.com>
2026-07-17 20:23:43 -0400

    chore: remove IDE and build artifacts from tracking
    Remove .idea/, cmake-build-debug/, __pycache__/, coverage.clean from git tracking. Update .gitignore to prevent re-occurrence.

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ian/projects/libzen
-# Build directory: /home/ian/projects/libzen/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
--- cmake-build-debug/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ian/projects/libzen/src
-# Build directory: /home/ian/projects/libzen/cmake-build-debug/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
